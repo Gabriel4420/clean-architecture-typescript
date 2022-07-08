604f304b597c2c0c6193a5e71fef7bb450e5febf
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-08 19:07:27 -0300

    add save purchases usecase

---
+++ src/domain/useCases/save-purchases.ts
@@ -0,0 +1,10 @@
+export interface SavePurchases {
+  save: (purchase: Array<SavePurchases.Params>) => Promise<void>
+}
+namespace SavePurchases {
+  export type Params = {
+    id: string
+    date: Date
+    value: number
+  }
+}
