ae647e8eb1727246b4cf30a8cbd3f408a8371ac9
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-08 22:16:36 -0300

    ensure LSP (LocalSavePurchases) will not insert new Cache if delete fails

--- src/data/usecases/save-purchases/local-save-purchases.ts
+++ src/data/usecases/save-purchases/local-save-purchases.ts
@@ -3,7 +3,7 @@ import { CacheStore } from '@/data/protocols/cache'
 export class LocalSavePurchases {
   constructor(private readonly cacheStore: CacheStore) {}
 
-  async save(): Promise<void> {
+  async save (): Promise<void> {
     this.cacheStore.delete('purchases')
   }
 }
