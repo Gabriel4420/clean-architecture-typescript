9ec13f3d4cc257eddb93da256edaafcb847d4c4e
Gabriel4420 <gabriel_rodrigues_perez@hotmail.com>
2022-07-09 07:09:53 -0300

    ensure LSP throws if CacheStore.insert throws

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -26,6 +26,12 @@ class CacheStoreSpy implements CacheStore {
       throw new Error()
     })
   }
+
+  simulateInsertError(): void {
+    jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
+      throw new Error()
+    })
+  }
 }
 
 const mockPurchases = (): Array<SavePurchases.Params> => [
@@ -82,5 +88,13 @@ describe('LocalSavePurchases', () => {
     expect(cacheStore.insertCallsCount).toBe(1)
     expect(cacheStore.insertKey).toBe('purchases')
     expect(cacheStore.insertValues).toEqual(purchases)
+  })
+
+  test('Should throw if insert throws', () => {
+    const { cacheStore, sut } = makeSut()
+    
+    cacheStore.simulateInsertError()
+    const promise = sut.save(mockPurchases())
+    expect(promise).rejects.toThrow()
   })
 })
